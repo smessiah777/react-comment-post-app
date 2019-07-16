15301d6c11721776eefaca5b2c91b85f4bad095a
smessiah777 <smessiah17@gmail.com>
2019-07-16 16:10:01 -0500

    time stamp and auto generated faker lorem comment content props created for CommentDetail component

--- client/src/components/CommentDetail.js
+++ client/src/components/CommentDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import faker from 'faker';
+
 
 const CommentDetail = props => {
     return (
         <div className="comment">
         <a href="/" className="avatar">
-            <img src={faker.image.avatar()} alt="avatar"/>
+            <img src={props.avatars} alt="avatar"/>
         </a>
         <div className="content">
             <a href="/" className="author">
@@ -13,11 +13,11 @@ const CommentDetail = props => {
             </a>
             <div className="metadata">
                 <span className="date">
-                    Today at 6:00pm
+                   {props.timeAgo}
                 </span>
             </div>
             <div className="text">
-                Nice blog post!
+                {props.content}
             </div>
         </div>
     </div>
